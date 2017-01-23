99e4b06e578cff40623f55d4040624856d6b5765
Phuong D. Pham <phamduyphuong10@gmail.com>
2017-01-23 15:49:21 +0700

    Emit newMessage on createMessage

--- server/server.js
+++ server/server.js
@@ -14,14 +14,14 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
   console.log('New user connected');
 
-  socket.emit('newMessage', {
-    from: 'Mai',
-    text: 'Love Phuong',
-    createAt: 123123
-  });
-
   socket.on('createMessage', (message) => {
     console.log('creageMessage', message);
+
+    io.emit('newMessage', {
+      from: message.from,
+      text: message.text,
+      createAt: new Date().getTime()
+    });
   });
 
   socket.on('disconnect', () => {
